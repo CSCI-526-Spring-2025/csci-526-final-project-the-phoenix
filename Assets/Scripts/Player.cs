72eae5f9732d77a00a6ec6dda16b609a4ad63195
Faerie Mattins <faeriemattins73191817@gmail.com>
2025-03-03 09:43:49 -0800

    Added win text with visual cues

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,12 +9,16 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isGrounded;
+    private int isGravityInverted;
 
     [SerializeField] private Clone cloneScript;
+    public GameObject winText;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isGravityInverted = 1;
+        isGrounded = true;
     }
 
     void Update()
@@ -28,8 +32,8 @@ public class Player : MonoBehaviour
         // Jump with up arrow key
         if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
+            Debug.Log("Jumping");
+            rb.AddForce(isGravityInverted * Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
 
@@ -42,11 +46,21 @@ public class Player : MonoBehaviour
         }
     }
 
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        // Check if player is no longer touching the ground
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = false;
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D collider)
     {
         // Check if player has reached the goal
         if (collider.gameObject.CompareTag("Finish"))
         {
+            winText.SetActive(true);
             Debug.Log("You win!");
         }
 
@@ -57,5 +71,11 @@ public class Player : MonoBehaviour
             cloneScript.gameObject.SetActive(true);
             cloneScript.resetPosition();
         }
+    }
+
+    public void invertGravity()
+    {
+        // Flag as invert gravity when called
+        isGravityInverted *= -1;
     }
 }
