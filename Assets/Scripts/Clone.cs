79abc9c2625a2987d49e1d4e923678c6fdf6bc95
Srivarshan <srivarshan02@gmail.com>
2025-03-09 22:27:42 -0700

    Add Gravity Portals and disable Universal Gravity Shift

--- Assets/Scripts/Clone.cs
+++ Assets/Scripts/Clone.cs
@@ -1,3 +1,87 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Clone : MonoBehaviour
+// {
+//     // Player script reference
+//     [SerializeField] private Player playerScript;
+
+//     private float moveSpeed = 5f;
+//     private float jumpForce = 5f;
+
+//     private Rigidbody2D rb;
+//     private bool isGrounded;
+//     private int isGravityInverted;
+//     private Vector2 initialPosition;
+//     private bool canJump;
+
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody2D>();
+//         isGravityInverted = 1;
+//         isGrounded = true;
+//         moveSpeed = playerScript.moveSpeed;
+//         jumpForce = playerScript.jumpForce;
+//         canJump = true;
+
+//         // Get the starting position of the clone
+//         initialPosition = transform.position;
+
+//         // Disable the clone object temporarily
+//         gameObject.SetActive(false);
+//     }
+
+//     void Update()
+//     {
+//         // Horizontal movement using A and D keys
+//         float moveDirection = 0;
+//         if (Input.GetKey(KeyCode.A)) moveDirection = -1;
+//         if (Input.GetKey(KeyCode.D)) moveDirection = 1;
+//         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+
+//         // Jump only if it's allowed in the scene
+//         if (canJump && Input.GetKeyDown(KeyCode.W) && isGrounded)
+//         {
+//             Debug.Log("Jumping");
+//             rb.AddForce(isGravityInverted * Vector2.up * jumpForce, ForceMode2D.Impulse);
+//         }
+//     }
+
+//     void OnCollisionEnter2D(Collision2D collision)
+//     {
+//         if (collision.gameObject.CompareTag("Floor"))
+//         {
+//             isGrounded = true;
+//         }
+//     }
+
+//     void OnCollisionExit2D(Collision2D collision)
+//     {
+//         if (collision.gameObject.CompareTag("Floor"))
+//         {
+//             isGrounded = false;
+//         }
+//     }
+
+//     public void resetPosition()
+//     {
+//         // Reset the clone's position to the initial position
+//         transform.position = initialPosition;
+//     }
+
+//     public void invertGravity()
+//     {
+//         Debug.Log("Gravity is inverted");
+//         isGravityInverted *= -1;
+//     }
+
+//     public void disableJump()
+//     {
+//         canJump = false;
+//     }
+// }
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +99,7 @@ public class Clone : MonoBehaviour
     private int isGravityInverted;
     private Vector2 initialPosition;
     private bool canJump;
+    private bool canToggleGravity = false;
 
     void Start()
     {
@@ -46,6 +131,12 @@ public class Clone : MonoBehaviour
             Debug.Log("Jumping");
             rb.AddForce(isGravityInverted * Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+        if (canToggleGravity && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Gravity Shifted");
+            invertGravity();
+            rb.gravityScale *= -1;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -61,6 +152,22 @@ public class Clone : MonoBehaviour
         if (collision.gameObject.CompareTag("Floor"))
         {
             isGrounded = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("GravityPortal"))
+        {
+            Debug.Log("Entered Gravity Portal");
+            canToggleGravity = true; // Allow gravity toggle while inside portal
+        }
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("GravityPortal"))
+        {
+            Debug.Log("Exited Gravity Portal");
+            canToggleGravity = false; 
         }
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,101 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement; 
+
+// public class Player : MonoBehaviour
+// {
+//     public float moveSpeed = 5f;
+//     public float jumpForce = 5f;
+
+//     private Rigidbody2D rb;
+//     private bool isGrounded;
+//     private int isGravityInverted;
+//     private bool canJump;
+
+//     [SerializeField] private Clone cloneScript;
+//     public GameObject winText;
+//     public GameObject arrow1;
+//     public GameObject arrow2;
+
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody2D>();
+//         isGravityInverted = 1;
+//         isGrounded = true;
+//         winText.SetActive(false);
+//         canJump = true;
+//     }
+
+//     void Update()
+//     {
+//         // Horizontal movement using left and right arrow keys
+//         float moveDirection = 0;
+//         if (Input.GetKey(KeyCode.LeftArrow)) moveDirection = -1;
+//         if (Input.GetKey(KeyCode.RightArrow)) moveDirection = 1;
+//         rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+
+//         // Jump only if it's allowed in the scene
+//         if (canJump && Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+//         {
+//             Debug.Log("Jumping");
+//             rb.AddForce(isGravityInverted * Vector2.up * jumpForce, ForceMode2D.Impulse);
+//         }
+//     }
+
+//     void OnCollisionEnter2D(Collision2D collision)
+//     {
+//         if (collision.gameObject.CompareTag("Floor"))
+//         {
+//             isGrounded = true;
+//         }
+//     }
+
+//     void OnCollisionExit2D(Collision2D collision)
+//     {
+//         if (collision.gameObject.CompareTag("Floor"))
+//         {
+//             isGrounded = false;
+//         }
+//     }
+
+//     void OnTriggerEnter2D(Collider2D collider)
+//     {
+//         if (collider.gameObject.CompareTag("Finish"))
+//         {
+//             LevelManager.Instance.TrackLevelCompletion(SceneManager.GetActiveScene().name, Time.timeSinceLevelLoad);
+//             winText.SetActive(true);
+//             Debug.Log("You win!");
+//             Invoke("LoadNextScene", 1f);
+//         }
+
+//         if (collider.gameObject.CompareTag("PlayerPlatform"))
+//         {
+//             Debug.Log("Cloning player");
+//             cloneScript.gameObject.SetActive(true);
+//             arrow2.SetActive(true);
+//             arrow1.SetActive(false);
+//             cloneScript.resetPosition();
+//         }
+//     }
+
+//     public void invertGravity()
+//     {
+//         isGravityInverted *= -1;
+//     }
+
+//     public void disableJump()
+//     {
+//         canJump = false;
+//     }
+
+//     private void LoadNextScene()
+//     {
+//         SceneManager.LoadScene("Level1");
+//     }
+// }
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,11 +110,10 @@ public class Player : MonoBehaviour
     private bool isGrounded;
     private int isGravityInverted;
     private bool canJump;
+    private bool canToggleGravity = false;
 
     [SerializeField] private Clone cloneScript;
     public GameObject winText;
-    public GameObject arrow1;
-    public GameObject arrow2;
 
     void Start()
     {
@@ -40,6 +137,12 @@ public class Player : MonoBehaviour
         {
             Debug.Log("Jumping");
             rb.AddForce(isGravityInverted * Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (canToggleGravity && Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Gravity Shifted");
+            invertGravity();
+            rb.gravityScale *= -1;
         }
     }
 
@@ -73,9 +176,23 @@ public class Player : MonoBehaviour
         {
             Debug.Log("Cloning player");
             cloneScript.gameObject.SetActive(true);
-            arrow2.SetActive(true);
-            arrow1.SetActive(false);
             cloneScript.resetPosition();
+        }
+
+        if (collider.gameObject.CompareTag("GravityPortal"))
+        {
+            Debug.Log("Entered Gravity Portal");
+            canToggleGravity = true;
+            
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("GravityPortal"))
+        {
+            Debug.Log("Exited Gravity Portal");
+            canToggleGravity = false; 
         }
     }
 
