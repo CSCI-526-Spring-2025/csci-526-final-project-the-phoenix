f607ad0f1ba86aa964c41d41114112dcb9297d25
Srivarshan <srivarshan02@gmail.com>
2025-03-24 19:24:56 -0700

    Refine Rotation Scripts

--- Assets/Scripts/BlockRotator.cs
+++ Assets/Scripts/BlockRotator.cs
@@ -7,6 +7,7 @@ public class SquareRotator : MonoBehaviour
     public void Rotate90Clockwise()
     {
         currentAngle += 90f;
+        currentAngle %= 360f;
         transform.rotation = Quaternion.Euler(0f, 0f, currentAngle);
     }
 }
--- Assets/Scripts/SwitchTrigger.cs
+++ Assets/Scripts/SwitchTrigger.cs
@@ -6,7 +6,7 @@ public class SwitchTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") || other.CompareTag("Clone"))
         {
             targetSquare?.Rotate90Clockwise();
         }
