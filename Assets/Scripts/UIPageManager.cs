4b178c217dca8390049bfebd0cc75e648db8e1d1
Swathi <swathiasok14@gmail.com>
2025-04-06 12:31:19 -0700

    Added start page

---
+++ Assets/Scripts/UIPageManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIPageManager : MonoBehaviour
+{
+    [SerializeField] private GameObject buttons;
+    [SerializeField] private GameObject modal;
+    [SerializeField] private GameObject levelSelector;
+
+    public void OpenInstructionModal()
+    {
+        buttons.SetActive(false);
+        modal.SetActive(true);
+    }
+
+    public void OpenLevelSelector()
+    {
+        buttons.SetActive(false);
+        levelSelector.SetActive(true);
+    }
+
+    public void CloseInstructionModal()
+    {
+        modal.SetActive(false);
+        buttons.SetActive(true);
+    }
+
+    public void CloseLevelSelector()
+    {
+        levelSelector.SetActive(false);
+        buttons.SetActive(true);
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
